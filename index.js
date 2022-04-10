2464138f3bb637ef9ad481389a4c655966966e35
soheiljafarnejad <jafarnejad.official@gmail.com>
2022-04-10 22:19:26 +0430

    multiple state

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const redux = require("redux");
 const createStore = redux.createStore;
 
 const BUY_CAKE = "BUY-CAKE";
+const BUY_ICE = "BUY-ICE";
 
 function buyCake() {
   return {
@@ -9,13 +10,23 @@ function buyCake() {
   };
 }
 
+function buyIce() {
+  return {
+    type: BUY_ICE,
+  };
+}
+
 const initState = {
   numberCake: 10,
+  numberIce: 20,
 };
+
 const reducer = (state = initState, action) => {
   switch (action.type) {
     case BUY_CAKE:
       return { ...state, numberCake: state.numberCake - 1 };
+    case BUY_ICE:
+      return { ...state, numberIce: state.numberIce - 1 };
     default:
       return state;
   }
@@ -25,9 +36,12 @@ const store = createStore(reducer);
 
 console.log("initial state", store.getState());
 
-store.subscribe(() => console.log("updated", store.getState()));
+const unsubscribe = store.subscribe(() =>
+  console.log("updated", store.getState())
+);
 
 store.dispatch(buyCake());
 store.dispatch(buyCake());
-store.dispatch(buyCake());
+store.dispatch(buyIce());
 
+unsubscribe();
